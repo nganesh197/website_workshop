579291ad92f348f4551237fbd3e6308e02e4e98f
nganesh197 <nganesh197@gmail.com>
2023-04-11 22:14:01 -0700

    Added in comments to css and js files

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var switchModes = document.getElementById("switchModes");
-// var sun = document.getElementById("sun-go");
-// var slider = document.getElementById("slider");
-
+// first three variables are constant, meaning they cannot be changed or modified further down in the code
+// document.querySelector() reads what is on the html page and looks for certain tags, ids, or classes and saves it to the variable.
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const navigation = document.querySelector('.header');
+
 let showMenu = false;
 
+// addEventListener waits for the user to do something on the webpage such as scrolling or clicking. Here when the user 
+// clicks on the X mark on the mobile version of the webpage, the javascript code will add the class "open" to the navigation bar.
 menuBtn.addEventListener('click', toggleMenu);
 
+// This controls if the menu is showed or not
 function toggleMenu() { 
     if(!showMenu){
         hamburger.classList.add('open');
